c0816eb22bbfab18883341afff3e985c1f562120
Administrator <Administrator@scw-PC>
2016-11-22 13:22:16 +0900

    추가 membercontroller class

---
+++ src/servlet/MemberController.java
@@ -0,0 +1,33 @@
+package servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/member.do")
+public class MemberController extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		execute(request, response);
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		request.setCharacterEncoding("EUC-KR");
+		execute(request, response);
+	}
+
+	public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String action = request.getParameter("action");
+		String viewPath = "";
+		
+		
+	}
+}
