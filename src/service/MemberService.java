13a0fe42d3104b89ccb092e995be6e08b6c19833
dmstjs930 <dmstjs930@naver.com>
2016-11-22 12:43:27 +0900

    Create MemberService

--- src/service/MemberService.java
+++ src/service/MemberService.java
@@ -1,7 +1,36 @@
 package service;
 
+import dao.MemberDAO;
+import vo.MemberVO;
+
 public class MemberService {
 	private static MemberService instance = new MemberService();
+	public static MemberService getInstance(){
+		return instance;
+	}
+	public MemberService(){}
+	private MemberDAO dao = MemberDAO.getInstance();
 	
-	public MemberService() {		}
+	public boolean join(MemberVO member){
+		MemberVO savedMember = dao.select(member.getId());
+		if(savedMember != null){
+			return false;
+		} else{
+			int result = dao.insert(member);
+			if(result ==1){
+				return true;
+			} else {
+				return false;
+			}
+		}
+	}
+	
+	public boolean login(String id, String password){
+		MemberVO savedMember = dao.select(id);
+		if(savedMember != null && savedMember.getPassword().equals(password)){
+			return true;
+		} else {
+			return false;
+		}
+	}
 }
