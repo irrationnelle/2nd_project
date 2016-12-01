9c22e4e0329a142de8ca2f6225fe1db011e4500d
Administrator <Administrator@scw-PC>
2016-12-01 17:48:04 +0900

    Update OrderController

--- src/service/CartService.java
+++ src/service/CartService.java
@@ -1,5 +1,17 @@
 package service;
 
-public class CartService {
+import dao.CartDAO;
 
+import dao.ProductDAO;
+
+public class CartService {
+	private static CartService instance = new CartService();
+	public static ProductService getInstance(){
+		return instance;
+	}
+	
+	private CartService(){}
+	private CartDAO dao = CartDAO.getInstance();
+
+	
 }
---
+++ src/servlet/OrderController.java
@@ -0,0 +1,65 @@
+package servlet;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import service.ProductService;
+import vo.OrderInfoVO;
+
+@WebServlet()
+public class OrderController extends HttpServlet {
+	private OrderService service = OrderService.getInstance();
+
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		execute(request, response);
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		request.setCharacterEncoding("EUC-KR");
+		execute(request, response);
+	}
+
+	protected void execute(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		String action = request.getParameter("action");
+		String viewPath = "";
+
+		switch (action) {
+		case "checkout":
+
+			String checkoutStr = request.getParameter("checkout");
+
+			if (checkoutStr != null && checkoutStr.length() >= 0) {
+				int checkout = Integer.parseInt(checkoutStr);
+			}
+
+			checkout = service.insertOrderInfo("checkout");
+
+			break;
+
+		case "showOrderInfo":
+			String showOrderInfoIdStr = request.getParameter("orderInfoId");
+
+			if (showOrderInfoIdStr != null && showOrderInfoIdStr.length() >= 0) {
+				int showOrderInfoId = Integer.parseInt(showOrderInfoIdStr);
+			}
+			
+			List<OrderInfoVO> orderInfo = service.selectOrderInfo(showOrderInfoId);
+			
+			
+			
+
+		}
+	}
+}
