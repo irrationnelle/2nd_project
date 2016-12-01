6c06e99fe6922fdda0d98ac92ba711feeabb5c18
dmstjs930 <dmstjs930@naver.com>
2016-12-01 15:50:11 +0900

    추가 CartController

---
+++ src/servlet/CartController.java
@@ -0,0 +1,74 @@
+package servlet;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import service.ProductService;
+import vo.CartVO;
+import vo.ProductVO;
+
+@WebServlet("/cart.do")
+public class CartController extends HttpServlet{
+	
+//	private CartService service = CartService.getInstance();
+	
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) 
+			throws ServletException, IOException {
+		execute(request, response);
+	}
+	
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) 
+			throws ServletException, IOException {
+		request.setCharacterEncoding("EUC-KR");
+		execute(request, response);
+	}
+	
+	protected void execute(HttpServletRequest request, HttpServletResponse response) 
+			throws ServletException, IOException {
+		
+		String action = request.getParameter("action");
+		String viewPath = "";
+		
+		switch (action) {
+		case "cart":
+	
+			String clickAddCartStr = request.getParameter("addCart");
+			
+			if(clickAddCartStr != null && clickAddCartStr.length()>=0) {
+				int clickAddCart = Integer.parseInt(clickAddCartStr);
+			}
+//			
+//			int insertCart = service.insertCart(clickAddCart);
+//			List<CartVO> cartList = service.showCartList(clickAddCart);
+			
+//			request.setAttribute("cartList", cartList);
+			viewPath = "cartList.jsp";
+			break;
+		
+		case "clearCart":
+			String clearCartStr = request.getParameter("clearCart");
+			
+			if(clearCartStr != null && clearCartStr.length()>=0) {
+				int clearCart = Integer.parseInt(clearCartStr);
+			}
+			
+//			int clearCart = service.deleteCart(clearCart);
+			
+//			request.setAttribute("cartList", cartList);
+			viewPath = "cartList.jsp";
+			break;
+			
+//		case "updateCart";
+			
+		}
+		
+	}
+}
